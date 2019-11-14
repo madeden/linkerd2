eefa2b81df1fc36781fe84df6bb7700012ae2d52
Samuel Cozannet <samuel.cozannet@madeden.com>
2019-11-14 08:33:29 +0100

    Add support for temporary GKE clusters for cloud integration #3717
    Problem: cloud_integration only supports persistent GKE cluster
    
    Solution: add support for handling lifecycle of GKE clusters
    
    Validation: Various runs of "fake" PRs, Commits and tagging to test all matrix elements
    
    Fixes #3717
    
    Signed-off-by: Samuel Cozannet <samuel.cozannet@madeden.com>

--- bin/_gcp.sh
+++ bin/_gcp.sh
@@ -14,14 +14,22 @@ install_gcloud() {
     fi
 }
 
+set_gcloud_project() {
+    # Switches to a new project or exits if called on the same project
+    project="$1"
+    zone="$2"
+
+    gcloud config set core/project "$project"
+    gcloud config set compute/zone "$zone"
+}
+
 set_gcloud_config() {
     project="$1"
     zone="$2"
     cluster="$3"
 
     gcloud auth activate-service-account --key-file .gcp.json
-    gcloud config set core/project "$project"
-    gcloud config set compute/zone "$zone"
+    set_gcloud_project "$project" "$zone"
     gcloud config set container/cluster "$cluster"
 }
 
@@ -36,5 +44,42 @@ get_k8s_ctx() {
         fi
     done
 
-    gcloud container clusters get-credentials "$cluster"
+    gcloud container clusters get-credentials -q "$cluster"
+}
+
+get_available_k8s_versions() {
+    # Returns a ; separated list of valid versions 
+    gcloud container get-server-config --zone=us-central1-f --format="value(validMasterVersions:sort=1)"
+}
+
+get_default_k8s_version() {
+    # Returns the default cluster version available
+    gcloud container get-server-config --zone=us-central1-f --format="value(defaultClusterVersion)"
+}
+
+create_cluster() {
+    cluster="$1"
+    k8s_version="$2"
+    cluster_size="$3"
+    machine_type="$4"
+
+    # Creates a k8s cluster according to a cluster config file
+    gcloud container clusters create "${cluster}" \
+        --cluster-version "${k8s_version}" \
+        --machine-type "${machine_type}" \
+        --num-nodes "${cluster_size}" \
+        --quiet \
+        2>/dev/null
+
+    # This should be a while look instead to make it more robust
+    sleep 5
+
+    # Use this cluster & set creds for k8s
+    get_k8s_ctx "${project}" "${zone}" "${cluster}"
+} 
+
+destroy_cluster() {
+    cluster="$1"
+
+    gcloud container clusters delete -q "${cluster}" 2>/dev/null
 }
